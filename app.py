0b8c7bfbc7133ca8002864e9f0eae8e5a6266f90
Camila Delgado <85705477+camidelgado@users.noreply.github.com>
2022-08-08 20:26:54 -0300

    Update app.py

--- app.py
+++ app.py
@@ -17,7 +17,7 @@ def result(country,variety, aroma,aftertaste,acidity,body,balance,moisture):
     cols=['country','variety', 'aroma','aftertaste','acidity','body','balance','moisture']
     data=[country,variety, aroma,aftertaste,acidity,body,balance,moisture]
     posted=pd.DataFrame(np.array(data).reshape(1,8),columns=cols)
-    loaded_model=pickle.load(open('../models/coffee_model.pkl','rb'))
+    loaded_model=pickle.load(open('coffee_model.pkl','rb'))
     result=loaded_model.predict(posted)
     text_result=result.tolist()[0]
     if text_result=='Yes':
